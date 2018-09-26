6d072a84214b0e29a8ecca619635d79fc9083b52
Sune <fengel@gmail.com>
2018-09-26 21:12:14 +0200

    Initial commit

---
+++ readme.md
@@ -0,0 +1,15 @@
+# Paralaxer
+
+## Introduction
+
+> A react component that will animate it's nested children based on scroll position
+
+## Code Samples
+
+```javascript
+Coming...
+```
+
+## Installation
+
+> yarn add or npm install 'paralaxer'
---
+++ src/index.js
@@ -0,0 +1,164 @@
+import React from 'react'
+
+class Paralaxer extends React.PureComponent {
+  constructor(props) {
+    super(props)
+    this.myRef = React.createRef()
+
+    this.state = {
+      scrollPos: 0,
+      scrollArea: { start: 0, end: 300 },
+      elements: null,
+    }
+  }
+
+  componentDidMount() {
+    const { top } = this.myRef.current.getBoundingClientRect()
+    const scrollArea = {
+      start:
+        this.props.start === 0
+          ? 0
+          : this.props.start || top - (this.props.tresholdStart || 0),
+
+      end:
+        this.props.end === 0
+          ? 0
+          : this.props.end || top + (this.props.tresholdEnd || 0),
+    }
+    // Run through all children and prepare from, to and style values
+    const elements = [this.props.children].map((el) => {
+      if (el.constructor.name === 'Object') {
+        return this.setupElementDefaults(el)
+      } else if (el.constructor.name === 'Array') {
+        return el.map((el) => {
+          return this.setupElementDefaults(el)
+        })
+      } else {
+        return null
+      }
+    })
+
+    this.setState({ scrollArea, elements })
+    window.addEventListener('scroll', this.onScroll)
+  }
+
+  onScroll = () => {
+    this.setState({ scrollPos: document.body.getBoundingClientRect().y })
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.onScroll)
+  }
+
+  setupElementDefaults(el) {
+    let animObj = {}
+    let highestPercent = 0
+    if (el.props && el.props.animation) {
+      // iterate the from object of the element
+      const regEx = /[-]{0,1}[\d]*[.]{0,1}[\d]+/g
+      Object.keys(el.props.animation).forEach((key) => {
+        let stylePattern = {}
+        let start = {}
+        const styleObj = el.props.animation[key]
+        Object.keys(styleObj).forEach((key) => {
+          stylePattern[key] = styleObj[key].toString().replace(regEx, '$')
+          if (styleObj[key].toString().match(regEx)) {
+            start[key] = styleObj[key]
+              .toString()
+              .match(regEx)
+              .map(Number)
+          } else {
+            start[key] = styleObj[key].toString()
+          }
+        })
+        highestPercent = key === 'from' ? 0 : key === 'to' ? 100 : key
+        animObj[highestPercent] = {
+          stylePattern,
+          start,
+          percent: highestPercent,
+        }
+      })
+      if (highestPercent < 100) {
+        animObj[100] = { ...animObj[highestPercent] }
+      }
+      return { animObj, ...el }
+    }
+    return el
+  }
+
+  calculatePercentageScrolled() {
+    const { scrollPos, scrollArea } = this.state
+
+    let scrollTop = scrollArea.start - scrollPos * -1
+    const scrollHeight = scrollArea.end - scrollArea.start
+    scrollTop = scrollTop > 0 ? 0 : scrollTop
+
+    const percent = (scrollTop / scrollHeight) * 100 * -1
+
+    return Math.round(percent < 0 ? 0 : percent > 100 ? 100 : percent)
+  }
+
+  getCalculatedStyles(el) {
+    if (!el.animObj) {
+      return {}
+    }
+    const percentScrolled = this.calculatePercentageScrolled()
+    let newStyle = {}
+    let fromObj = null
+    let toObj = null
+    for (var percent in el.animObj) {
+      if (percent <= percentScrolled) {
+        fromObj = el.animObj[percent]
+      }
+      if (percent >= percentScrolled) {
+        toObj = el.animObj[percent]
+        break
+      }
+    }
+
+    for (var key in fromObj.stylePattern) {
+      let style = `${fromObj.stylePattern[key]}`
+      for (let i = 0; i < fromObj.start[key].length; i++) {
+        const from = fromObj.start[key][i]
+        const to = toObj.start[key][i]
+        let diff = Math.abs(from - to)
+        const interval = toObj.percent - fromObj.percent
+        const scrolled =
+          fromObj.percent > 0
+            ? percentScrolled - fromObj.percent
+            : percentScrolled
+
+        const interValScrolled = Math.abs((scrolled / interval) * 100 || 0)
+        let newPos = 0
+        if (from > to) {
+          newPos = to + (diff - (interValScrolled / 100) * diff)
+        } else {
+          newPos = to - (diff - (interValScrolled / 100) * diff)
+        }
+        style = style.toString().replace('$', newPos)
+      }
+      newStyle[key] = style
+    }
+    return newStyle
+  }
+
+  renderChildren() {
+    return React.Children.map(this.state.elements, (child) => {
+      return React.cloneElement(child, {
+        style: { ...child.props.style, ...this.getCalculatedStyles(child) },
+      })
+    })
+  }
+
+  render() {
+    return (
+      <div
+        className={`Paralax ${this.props.className || ''}`}
+        style={this.props.styles}
+        ref={this.myRef}>
+        {this.renderChildren()}
+      </div>
+    )
+  }
+}
+export default Paralaxer
